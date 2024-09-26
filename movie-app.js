235b0076bd03a44e70afa0b8f6262b64cf7427e6
Arjun <simplegamerofficial97@gmail.com>
2024-09-26 23:02:39 +0530

    practiced localstorage and simplified code with rednder f

--- movie-app.js
+++ movie-app.js
@@ -1,26 +1,32 @@
-/*
-fetchData();
-async function fetchData(){
-  
+
+const bodyContainer = document.querySelector(".body-container");
+const searchBtn = document.querySelector("#search-btn");
+
+initialLoad();
+search();
+
+//when webpage load for the first time
+async function initialLoad(){
   try{
-    const response = await fetch("https://api.tvmaze.com/search/shows?q=girls");
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=marvel`);
+
     if(!response.ok){
       throw new Error("Could not find resource!");
     }
-    const data = await response.json();
-    console.log(data);
-    const imageEl = document.querySelector("img");
-    const posterUrl = data.Poster;
-    imageEl.src = posterUrl;
-    
+    const queryData = await response.json();
+    //to store querydata in localstorage
+    localStorage.setItem('urlQueryData', JSON.stringify(queryData));
+    const localData = JSON.parse(localStorage.getItem('urlQueryData')); 
+    console.log(localData)
+    renderData(localData); //passing localdata to render f
+    localStorage.clear();//to clear localstrorage once loaded
   }
   catch(err){
     console.error(err);
   }
-*/  
-const bodyContainer = document.querySelector(".body-container");
-const searchBtn = document.querySelector("#search-btn");
+}
 
+function search(){
 //to get search input
 searchBtn.addEventListener('click', function() {
   const inputText = document.querySelector("input");
@@ -32,13 +38,13 @@ searchBtn.addEventListener('click', function() {
     alert("Must enter a name of the Movie/TV show!")
   }
 });
-
+}
 
 // to get a result for searchQuery
 async function fetchQueryData(searchQuery){
   try{
     const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchQuery}`);
-
+    console.log(response);
     if(!response.ok){
       throw new Error("Could not find resource!");
     }
@@ -46,29 +52,29 @@ async function fetchQueryData(searchQuery){
     const queryData = await response.json();
     console.log(queryData);
     console.log(searchQuery);
-    queryData.forEach(item => {
-      const url = item.show.image.medium;
-      const title = item.show.name;
-      const rating = item.show.rating.average;
-      const movieList = document.createElement('div');
-      movieList.classList.add('movies-list');
-      movieList.innerHTML = 
-        `<img src=${url}></img>
-        <div class="movie-info">
-        <h3>${title}</h3>
-        <span>${rating}</span>
-        </div>`;
-      bodyContainer.appendChild(movieList);
-    });
+
+    //Clear the previous content before displaying new results
+    bodyContainer.innerHTML = '';
+    renderData(queryData);
   }
   catch(err){
     console.error(err);
   }
 }
 
-/*
-      const posterUrl = movie.show.image.medium;
-      const imageEl = document.createElement("img");
-      imageEl.src = posterUrl;
-      bodyContainer.appendChild(imageEl);
-    */
+function renderData(data) {
+  data.forEach(item => {
+    const url = item.show.image.medium;
+    const title = item.show.name;
+    const rating = item.show.rating.average;
+    const movieList = document.createElement('div');
+    movieList.classList.add('movies-list');
+    movieList.innerHTML = 
+      `<img src=${url}></img>
+      <div class="movie-info">
+      <h3>${title}</h3>
+      <span>${rating}</span>
+      </div>`;
+    bodyContainer.appendChild(movieList);
+  });
+}
